83279489793d913b61721e31556fd0c2c32b2b0d
Sam Hulleman <109807@st-maartenscollege.nl>
2020-05-27 12:20:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# maak hier de hele enigma machine in python :D
+
+# beginnen met het progammeren van de rotors
+
+import alphabet
+alphabet = "abcdefghijklmnopqrstuwvxyz"
+
+StandRotorA = alphabet with alphabet.index
+StandRotorB = alphabet with alphabet.index
+StandRotorC = alphabet with alphabet.index
+LetterBoodschap = alphabet.index()
+
+def NieuweLetter (LetterBoodschap, StandRotorA, StandRotorB, StandRotorC)
+  for LetterBoodschap
+    if StandRotorA == 25
+      return StandRotorB + 1
+    if StandRotorB == 25
+      return StandRotorC + 1  
+  return StandRotorA + 1
+ 
+  for LetterBoodschap in StandRotorA
+  return StandRotorA(alphabet.index)
+
+  for StandRotorA(alphabet.index) in StandRotorB
+  return StandRotorB(alphabet.index)
+
+  for StandRotorB(alphabet.index) in StandRotorC
+  return StandRotorC(alphabet.index)
+print NieuweLetter(int())
+ 
+input = LetterBoodschap
+input = StandRotorA
+input = StandRotorB
+input = StandRotorC
+  
+   
